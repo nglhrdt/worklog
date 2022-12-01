6ee972d03e43d71f7324625467931a21882b19bd
Simon Engelhardt <se@inga-hameln.de>
2022-12-01 14:32:38 +0100

    use atlassion icon for integrations

--- src/app/components/profile/profile.component.ts
+++ src/app/components/profile/profile.component.ts
@@ -2,19 +2,22 @@ import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { Auth, User } from '@angular/fire/auth';
 import { RouterLink } from '@angular/router';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { LogoutComponent } from '../logout/logout.component';
+import { faAtlassian } from "@fortawesome/free-brands-svg-icons";
 
 @Component({
   selector: 'app-profile',
   standalone: true,
-  imports: [CommonModule, LogoutComponent, RouterLink],
+  imports: [CommonModule, LogoutComponent, RouterLink, FontAwesomeModule],
   templateUrl: './profile.component.html',
   styleUrls: ['./profile.component.scss']
 })
 export class ProfileComponent {
   user: User;
+  integrationIcon = faAtlassian;
 
-  constructor(auth: Auth) { 
+  constructor(auth: Auth) {
     this.user = auth.currentUser!;
   }
 }
