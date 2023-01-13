6b7835a5d957bce17a58d4b852d876d368d9a862
Simon Engelhardt <se@inga-hameln.de>
2023-01-13 15:31:55 +0100

    show location icon

--- src/app/components/activity-details/activity-details.component.ts
+++ src/app/components/activity-details/activity-details.component.ts
@@ -2,7 +2,8 @@ import { CommonModule } from '@angular/common';
 import { Component, Input } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
-import { faLaptopCode, faUsersBetweenLines } from '@fortawesome/free-solid-svg-icons';
+import { faHome, faLaptopCode, faUsersBetweenLines } from '@fortawesome/free-solid-svg-icons';
+import { faBuilding } from '@fortawesome/free-regular-svg-icons';
 import { Activity } from 'src/app/models/activity';
 import { MinutesToHoursAndMinutesPipe } from 'src/app/pipes/minutes-to-hours-and-minutes.pipe';
 
@@ -18,4 +19,6 @@ export class ActivityDetailsComponent {
   @Input() activity!: Activity;
   development = faLaptopCode;
   meeting = faUsersBetweenLines;
+  mobile = faHome;
+  company = faBuilding;
 }
