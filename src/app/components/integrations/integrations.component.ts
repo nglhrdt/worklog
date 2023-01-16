08b42a7ae6b53e296a3a0f963351ef846531f5c8
Simon Engelhardt <se@inga-hameln.de>
2023-01-16 14:01:41 +0100

    redirect after successful update

--- src/app/components/integrations/integrations.component.ts
+++ src/app/components/integrations/integrations.component.ts
@@ -3,6 +3,7 @@ import { Component } from '@angular/core';
 import { Auth } from '@angular/fire/auth';
 import { doc, docData, DocumentReference, Firestore, updateDoc } from '@angular/fire/firestore';
 import { FormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
 import { map, Observable } from 'rxjs';
 import { userConverter } from 'src/app/converters/user-converter';
 import { Integration, JiraIntegration, User } from 'src/app/models/user';
@@ -17,12 +18,12 @@ import { Integration, JiraIntegration, User } from 'src/app/models/user';
 export class IntegrationsComponent {
   integration$: Observable<JiraIntegration>;
 
-  constructor(private firestore: Firestore, private auth: Auth) {
+  constructor(private firestore: Firestore, private auth: Auth, private router: Router) {
     this.integration$ = docData(this.getUserDoc()).pipe(map((user: User) => (user.integrations.jira as JiraIntegration) ?? new JiraIntegration()))
   }
 
   saveIntegration(integration: Integration): void {
-    updateDoc(this.getUserDoc(), { integrations: { jira: { ...integration } } });
+    updateDoc(this.getUserDoc(), { integrations: { jira: { ...integration } } }).then(() => this.router.navigate(['']));
   }
 
   private getUserDoc(): DocumentReference<User> {
